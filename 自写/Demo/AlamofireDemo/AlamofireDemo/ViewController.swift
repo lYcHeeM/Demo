730b1accf0480482507a727f857e12a309185d8e
luozhijun <luo_zhijun@163.com>
2017-11-24 17:23:43 +0800

    regular restore

--- 自写/Demo/AlamofireDemo/AlamofireDemo/ViewController.swift
+++ 自写/Demo/AlamofireDemo/AlamofireDemo/ViewController.swift
@@ -38,16 +38,21 @@ class ViewController: UIViewController {
         let httpHeader: HTTPHeaders = [
             "Authorization": "Bearer \(token)"
         ]
+        let clinicId = "a018f483-7002-4baa-ab90-604bb27d721b"
+        let userId = "691"
+        let imgData = UIImagePNGRepresentation(#imageLiteral(resourceName: "Shake_Received_Icon"))
+        let imgBase64 = imgData?.base64EncodedString(options: .endLineWithCarriageReturn) ?? ""
+        let serialNum = "sdaf1lj1l3jlj1111"
         let params = [
-            "serialNum": "",
-            "idNo": "",
-            "photoStr": "",
-            "name": "",
-            "clinicId": "",
-            "userId": "",
-            "mobileNo": ""
+            "serialNum": serialNum,
+            "idNo": "360782199301050212",
+            "photoStr": imgBase64,
+            "name": "zhangsan",
+            "clinicId": clinicId,
+            "userId": userId,
+            "mobileNo": "13333333333"
         ]
-        request("https://opentest.pawjzs.com/i/open/faceRecognition/compare.json", method: .get, parameters: params, headers: httpHeader).response { (JSON) in
+        request("https://opentest.pawjzs.com/i/open/faceRecognition/compare.json?serialNum=\(serialNum)&channelType=QHZX", method: .post, parameters: params, encoding: JSONEncoding.default, headers: httpHeader).responseJSON { (JSON) in
             print(JSON)
         }
     }
